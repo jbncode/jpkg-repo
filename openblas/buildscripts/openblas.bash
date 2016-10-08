003767fbeb530b856d39fdc4396211e26e2ce95e
Joseph B. Nicholas <jbncode@gmail.com>
2016-10-08 00:12:55 -0400

    Fixed various bugs on Darwin

--- openblas/buildscripts/openblas.bash
+++ openblas/buildscripts/openblas.bash
@@ -12,8 +12,14 @@ function src_install {
     jmake -j1 PREFIX="$PACKAGE_INSTALL_DIR" install
 
     cd "$PACKAGE_INSTALL_DIR"/lib*
-    ln -s libopenblas.a libblas.a
-    ln -s libopenblas.a liblapack.a
-    ln -s libopenblas.so libblas.so
-    ln -s libopenblas.so liblapack.so
+    ln -sf libopenblas.a libblas.a
+    ln -sf libopenblas.a liblapack.a
+    if test -e libopenblas.so; then
+        ln -sf libopenblas.so libblas.so
+        ln -sf libopenblas.so liblapack.so
+    fi
+    if test -e libopenblas.dylib; then
+        ln -sf libopenblas.dylib libblas.dylib
+        ln -sf libopenblas.dylib liblapack.dylib
+    fi
 }
