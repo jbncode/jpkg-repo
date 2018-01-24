c270ed46073eb17539f7e84c9e8c1c58c2d8cad0
Joseph B. Nicholas <joseph.nicholas@emergentspace.com>
2018-01-24 13:50:38 -0500

    hdf5: Added fortran2003 flag

--- hdf5/buildscripts/hdf5.bash
+++ hdf5/buildscripts/hdf5.bash
@@ -2,5 +2,6 @@ function src_configure {
     jconfigure \
             $(use_enable zlib) \
             $(use_enable cxx) \
+            $(use_enable fortran2003) \
             --enable-fortran
 }
